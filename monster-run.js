4e62de7002e6267b5e572e0709c29796b67b6a18
nicolscc <77124676+nicolscc@users.noreply.github.com>
2021-01-07 15:21:48 -0500

    added sprites with movement

--- monster-run.js
+++ monster-run.js
@@ -1,7 +1,33 @@
+let img;
+let x = 100;
+let y = 100;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1000, 600);
+    img = loadImage('art/sprite.png',8,8);
 }
 
 function draw() {
-    background(20, 20, 50);
+    if (keyIsDown(LEFT_ARROW)) {
+        x -= 5;
+        img = loadImage('art/sprite-5.png',8,8);
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+        x += 5;
+        img = loadImage('art/sprite-4.png',8,8);
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+        y -= 5;
+        img = loadImage('art/sprite-1.png',8,8);
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+        y += 5;
+    }
+
+    clear();
+    background(100, 100, 250);
+    image(img, x, y);
 }
